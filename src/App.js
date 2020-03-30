c3c563d660413271517bb1637fdc3b73577d0402
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2020-03-30 23:16:58 +0700

    add PokemonDetail component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import PokemonTable from './PokemonTable.js';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="title">
+          Test Bulma cuy
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>yang ini ga bulma</p>
+        <PokemonTable />
       </header>
     </div>
   );
---
+++ src/PokemonDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PokemonTable from './PokemonTable.js';
+
+function PokemonDetail() {
+  return (
+    <div className="container">
+      <PokemonTable />
+    </div>
+  );
+}
+
+export default PokemonDetail;
