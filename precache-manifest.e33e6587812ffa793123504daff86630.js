d87c427e663d6a78173fbbf1ddb35f36b5026f8b
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2020-04-03 11:33:32 +0700

    Updates

---
+++ precache-manifest.e33e6587812ffa793123504daff86630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffcc9d3a78515f34d4341cf53adf522",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "478778b99adec8bdfacd",
+    "url": "/pokemon/static/css/main.c35d7e7f.chunk.css"
+  },
+  {
+    "revision": "b0f9123a39c4fce289eb",
+    "url": "/pokemon/static/js/2.f7c6ffe2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon/static/js/2.f7c6ffe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478778b99adec8bdfacd",
+    "url": "/pokemon/static/js/main.a98c288b.chunk.js"
+  },
+  {
+    "revision": "e1366ef5cfb767bc3a5a",
+    "url": "/pokemon/static/js/runtime-main.2bfe477e.js"
+  }
+]);
